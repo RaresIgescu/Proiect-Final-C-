3843eccc5f713c7c4eb885ac6f48f754bf976437
RaresIgescu <raresigescu@gmail.com>
2025-02-26 16:38:38 +0200

    Added Main View for main page on website

--- ArticlesApp/Program.cs
+++ ArticlesApp/Program.cs
@@ -49,7 +49,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Articles}/{action=Index}/{id?}");
+    pattern: "{controller=Articles}/{action=Main}/{id?}");
 app.MapRazorPages();
 
 app.Run();
